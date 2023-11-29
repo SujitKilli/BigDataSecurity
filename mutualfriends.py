20aecc16b5677661f56cf6b9f6d4827b34d3cae6
SujitKilli <63903253+SujitKilli@users.noreply.github.com>
2023-11-29 14:05:59 -0600

    Update mutualfriends.py

--- mutualfriends.py
+++ mutualfriends.py
@@ -1,40 +1,23 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 
 # Create a Spark session
-spark = SparkSession.builder.appName("MutualFriends").getOrCreate()
+spark = SparkSession.builder.appName("example").getOrCreate()
 
-# Read the data from the text file
-data_path = "./mutual.txt"  # Replace with the actual path to your file
-friends_info = spark.read.text(data_path)
+# Sample data
+data = [("John", 28), ("Alice", 22), ("Bob", 30), ("Charlie", 25)]
+columns = ["Name", "Age"]
 
-# Map function for parsing the file
-def create_friend_list(line):
-    user_friends = line.value.split('\t')
-    user_id = int(user_friends[0])
-    friends = []
-    if len(user_friends) > 1:
-        for fr in user_friends[1].split(','):
-            try:
-                friends.append(int(fr))
-            except:
-                print(fr)
-    return (user_id, friends)
+# Create a DataFrame
+df = spark.createDataFrame(data, columns)
 
-# Apply the map function to create a DataFrame of user and their friends
-user_friends = friends_info.rdd.map(create_friend_list).toDF(["user", "friends"])
+# Show the DataFrame
+df.show()
 
-# Create a DataFrame of friend pairs
-friend_pairs = user_friends.select("user", col("friends").alias("friend"))
+# Perform a simple transformation
+df_filtered = df.filter(df["Age"] > 25)
 
-# Self-join to get pairs of friends
-friend_pairs = friend_pairs.join(friend_pairs, "user", "inner").toDF("user", "friend1", "friend2")
+# Show the transformed DataFrame
+df_filtered.show()
 
-# Filter out rows where friend1 is not equal to friend2
-mutual_friends = friend_pairs.filter(col("friend1") != col("friend2"))
-
-# Display the result
-mutual_friends.show(truncate=False)
-
-# Stop Spark session
+# Stop the Spark session
 spark.stop()
